2d37beba61703367cd6c0d9cb01f2298ae5a0e31
Keita-Kawauchi <kerokero5224@icloud.com>
2021-04-17 08:09:51 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,8 @@
    def create
      @book=Book.new(book_params)
      if @book.save
-      flash[:notice] = "successfully" 
+      redirect_to book_path(@book)
+      flash[:notice] = "successfully"
      else @books=Book.all
        render action: :index
      end
@@ -32,7 +33,7 @@
    def update
      @book=Book.find(params[:id])
      if @book.update(book_params)
-      flash[:notice] = "successfully" 
+      flash[:notice] = "successfully"
       redirect_to book_path(@book)
      else
       render "edit"
